1c1461897176691845c37351218d3b0eff7ac591
Irina Kucherenko <raito-werner@yandex.ru>
2024-09-04 19:55:35 +0300

    feat: add dependencies, add annotations

--- stats/stats-dto/src/main/java/ru/practicum/dto/ServiceHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/ServiceHitDto.java
@@ -1,5 +1,9 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +16,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServiceHitDto {
+
+    @NotBlank(message = "App cannot be blank or empty")
     private String app;
+
+    @NotBlank(message = "URI cannot be blank or empty")
     private String uri;
+
+    @Pattern(regexp = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$", message = "Invalid IP format")
+    @NotBlank(message = "IP cannot be blank or empty")
     private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "Timestamp cannot be empty")
     private LocalDateTime timestamp;
 }
