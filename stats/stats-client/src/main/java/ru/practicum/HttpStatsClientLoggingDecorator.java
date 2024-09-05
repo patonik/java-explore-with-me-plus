eb6d1dce0d3ca620a72a272211452eab979ccdb9
Safronov Igor <safronovigor1c@yandex.ru>
2024-09-05 14:14:25 +0300

    refactor: return types to Optional in logging decorator

--- stats/stats-client/src/main/java/ru/practicum/HttpStatsClientLoggingDecorator.java
+++ stats/stats-client/src/main/java/ru/practicum/HttpStatsClientLoggingDecorator.java
@@ -17,7 +17,7 @@ public class HttpStatsClientLoggingDecorator implements HttpStatsClient {
     @Override
     public <R> Optional<R> getStats(String start, String end, List<String> uris, boolean unique, Class<R> responseType) {
         log.info("Getting stats from {} to {} for URIs: {}", start, end, uris.toString());
-        R result = delegate.getStats(start, end, uris, unique, responseType);
+        Optional<R> result = delegate.getStats(start, end, uris, unique, responseType);
         log.info("Stats received: {}", result);
         return result;
     }
@@ -25,7 +25,7 @@ public class HttpStatsClientLoggingDecorator implements HttpStatsClient {
     @Override
     public <T, R> Optional<R> sendHit(T hit, Class<R> responseType) {
         log.info("Sending hit: {}", hit);
-        R result = delegate.sendHit(hit, responseType);
+        Optional<R> result = delegate.sendHit(hit, responseType);
         log.info("Hit response: {}", result);
         return result;
     }
