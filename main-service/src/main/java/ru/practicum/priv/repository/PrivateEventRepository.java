f44c7b7d02b5d505e812a9156dd5ae81f37dcf8e
NikolayPatov <nikolay.patov@gmail.com>
2024-09-09 16:28:57 +0300

    feat: event service getMyEvents implementation

--- main-service/src/main/java/ru/practicum/priv/repository/PrivateEventRepository.java
+++ main-service/src/main/java/ru/practicum/priv/repository/PrivateEventRepository.java
@@ -1,11 +1,14 @@
 package ru.practicum.priv.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.dto.event.request.RequestCount;
 import ru.practicum.dto.event.request.Status;
 import ru.practicum.model.Event;
+
+import java.util.List;
 
 
 @Repository
@@ -16,4 +19,6 @@ public interface PrivateEventRepository extends JpaRepository<Event, Long> {
         where r.event.id=:eventId and r.status=:status
         """)
     RequestCount getRequestCountByEventAndStatus(Long eventId, Status status);
+
+    List<Event> findAllByInitiatorIdOrderByCreatedOnAsc(Long userId, Pageable pageable);
 }
--- main-service/src/main/java/ru/practicum/priv/service/PrivateEventService.java
+++ main-service/src/main/java/ru/practicum/priv/service/PrivateEventService.java
@@ -1,11 +1,16 @@
 package ru.practicum.priv.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.HttpStatsClient;
+import ru.practicum.dto.StatResponseDto;
 import ru.practicum.dto.event.EventFullDto;
 import ru.practicum.dto.event.EventFullDtoMapper;
 import ru.practicum.dto.event.EventShortDto;
+import ru.practicum.dto.event.EventShortDtoMapper;
 import ru.practicum.dto.event.NewEventDto;
 import ru.practicum.dto.event.NewEventDtoMapper;
 import ru.practicum.dto.event.State;
@@ -17,6 +22,9 @@ import ru.practicum.priv.repository.PrivateCategoryRepository;
 import ru.practicum.priv.repository.PrivateEventRepository;
 import ru.practicum.priv.repository.PrivateUserRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -27,6 +35,8 @@ public class PrivateEventService {
     private final PrivateCategoryRepository privateCategoryRepository;
     private final NewEventDtoMapper newEventDtoMapper;
     private final EventFullDtoMapper eventFullDtoMapper;
+    private final EventShortDtoMapper eventShortDtoMapper;
+    private final HttpStatsClient httpStatsClient;
 
     @Transactional
     public EventFullDto addEvent(Long userId, NewEventDto newEventDto) {
@@ -43,7 +53,33 @@ public class PrivateEventService {
         return eventFullDtoMapper.toDto(event, confirmedRequests);
     }
 
+    @Transactional(readOnly = true)
     public List<EventShortDto> getMyEvents(Long userId, Integer from, Integer size) {
-        return null;
+        Pageable pageable = PageRequest.of(from, size);
+        List<Event> eventList = privateEventRepository.findAllByInitiatorIdOrderByCreatedOnAsc(userId, pageable);
+        Params params = getParams(eventList);
+        List<StatResponseDto> statResponseDto =
+            httpStatsClient.getStats(params.start(), params.end(), params.uriList(), false);
+        long[] hitList =
+            statResponseDto
+                .stream()
+                .sorted(Comparator.comparingLong(x -> Long.parseLong(x.getUri().split("/")[2])))
+                .mapToLong(StatResponseDto::getHits)
+                .toArray();
+        List<EventShortDto> eventShortDtos = new ArrayList<>();
+        for (int i = 0; i < hitList.length; i++) {
+            eventShortDtos.add(eventShortDtoMapper.toDto(eventList.get(i), hitList[i]));
+        }
+        return eventShortDtos;
+    }
+
+    private static Params getParams(List<Event> eventList) {
+        String start = String.valueOf(eventList.getFirst().getCreatedOn());
+        String end = String.valueOf(LocalDateTime.now());
+        List<String> uriList = eventList.stream().map(x -> "/events/" + x.getId()).toList();
+        return new Params(start, end, uriList);
+    }
+
+    private record Params(String start, String end, List<String> uriList) {
     }
 }
