6fd5a4cd4725fb6a997aeb659863b5f2cc99ab18
Safronov Igor <SafronovIgor1C@yandex.ru>
2024-09-09 09:33:39 +0300

    ref: Add mapping for requester in RequestDtoMapper

--- main-service/src/main/java/ru/practicum/dto/event/request/RequestDtoMapper.java
+++ main-service/src/main/java/ru/practicum/dto/event/request/RequestDtoMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.dto.event.request;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.model.Request;
 
 @Mapper(componentModel = "spring")
 public interface RequestDtoMapper {
+    @Mapping(source = "requester.id", target = "requester")
     ParticipationRequestDto toParticipationRequestDto(Request request);
 }
