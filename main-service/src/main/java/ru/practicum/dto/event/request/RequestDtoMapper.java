ab3be60deb0bf24a61ad1fb2f36d5bf6676e3f11
Safronov Igor <SafronovIgor1C@yandex.ru>
2024-09-09 09:21:19 +0300

    ref: Remove `toRequest` mapping from RequestDtoMapper

--- main-service/src/main/java/ru/practicum/dto/event/request/RequestDtoMapper.java
+++ main-service/src/main/java/ru/practicum/dto/event/request/RequestDtoMapper.java
@@ -1,13 +1,9 @@
 package ru.practicum.dto.event.request;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.practicum.model.Request;
 
 @Mapper(componentModel = "spring")
 public interface RequestDtoMapper {
-    @Mapping(target = "id", ignore = true)
-    Request toRequest(ParticipationRequestDto requestDto);
-
     ParticipationRequestDto toParticipationRequestDto(Request request);
 }
